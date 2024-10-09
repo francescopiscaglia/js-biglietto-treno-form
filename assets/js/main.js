67b022ae30be01de635711f4cc7d06417418155c
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-09 14:30:27 +0200

    add two input type text and one input type submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,13 @@
 // creo i due prompt per le risposte dell'utente
-
-const numberOfKm = Number(prompt("Ciao, quanti km dovrai percorrere"));
-const passengerAge = Number(prompt("Quanti anni ha il passeggero?"));
+// const numberOfKm = Number(prompt("Ciao, quanti km dovrai percorrere"));
+// const passengerAge = Number(prompt("Quanti anni ha il passeggero?"));
 // console.log(numberOfKm, userAge);
 
 // prezzo del biglietto al km
 const priceAtKm = 0.21;
 
-
 // creo una variabile per il prezzo finale
 let ticketFinalPrice
-
 
 /**
  * 
@@ -23,13 +20,13 @@ function ticketPrice(numbOne, numbTwo) {
     // prezzo base
     let basePrice = numberOfKm * priceAtKm;
 
-    if (numbTwo < 18) {
+    if (numbTwo < 18) { // se minorenne
         ticketFinalPrice = basePrice - (basePrice * 0.2);
-
-    } else if (numbTwo >= 65) {
+    
+    } else if (numbTwo >= 65) { // se over 65
         ticketFinalPrice = basePrice - (basePrice * 0.4);
 
-    } else {
+    } else { // altrimenti nessuno sconto
         ticketFinalPrice = basePrice;
     }
 
