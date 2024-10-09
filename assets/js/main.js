d4f33a019c01cd7ddb1dff3937845d38608f8806
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-09 14:27:17 +0200

    add function for calculate the finale price

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,39 @@
 // creo i due prompt per le risposte dell'utente
 
 const numberOfKm = Number(prompt("Ciao, quanti km dovrai percorrere"));
-const userAge = Number(prompt("Quanti anni ha il passeggero?"));
+const passengerAge = Number(prompt("Quanti anni ha il passeggero?"));
+// console.log(numberOfKm, userAge);
 
-console.log(numberOfKm, userAge);
+// prezzo del biglietto al km
+const priceAtKm = 0.21;
+
+
+// creo una variabile per il prezzo finale
+let ticketFinalPrice
+
+
+/**
+ * 
+ * @param {number} numberOne number of the km 
+ * @param {number} numberTwo passenger age 
+ * @returns {number} final price
+*/
+function ticketPrice(numbOne, numbTwo) {
+
+    // prezzo base
+    let basePrice = numberOfKm * priceAtKm;
+
+    if (numbTwo < 18) {
+        ticketFinalPrice = basePrice - (basePrice * 0.2);
+
+    } else if (numbTwo >= 65) {
+        ticketFinalPrice = basePrice - (basePrice * 0.4);
+
+    } else {
+        ticketFinalPrice = basePrice;
+    }
+
+    return ticketFinalPrice.toFixed(2);
+}
+
+console.log(`Il prezzo finale del biglietto è ${ticketPrice(numberOfKm, passengerAge)}€`);
