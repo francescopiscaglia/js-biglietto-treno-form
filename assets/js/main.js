d8e15c2d0ccde092c9b7e56a495e1707ae83c26d
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-09 14:07:33 +0200

    add prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+// creo i due prompt per le risposte dell'utente
+
+const numberOfKm = Number(prompt("Ciao, quanti km dovrai percorrere"));
+const userAge = Number(prompt("Quanti anni ha il passeggero?"));
+
+console.log(numberOfKm, userAge);
