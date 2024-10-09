ff9b63e9dadd4e4da6155ec52ccbb4176e371ea3
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-09 17:46:17 +0200

    add comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
-// seleziono il valore dell'input
+// seleziono il valore dell'input del form
 const numberOfKmEl = document.getElementById("number-of-km");
 const passengerAgeEl = document.getElementById("passenger-age");
 const passengerNameEl = document.getElementById("passengerName");
 
-// seleziono l'input submit dalla DOM
+// seleziono il form dalla DOM
 const formEL = document.querySelector("form");
 
-// seleziono la card body dalla DOM
+// seleziono la card dalla DOM
 const cardsEL = document.querySelector(".col-7");
 
 // seleziono le classi colonne da modificare
 const colEl = document.querySelector(".col-12");
-console.log(colEl);
 
 // event listener
 formEL.addEventListener("submit", function (e) {
@@ -35,7 +34,8 @@ formEL.addEventListener("submit", function (e) {
     let typeOfOffer;
 
     // controllare se l'età del passeggero e i km sono maggiori di 0
-    if (passengerAgeValue > 0 && numberOfKmValue > 0) {
+    if (passengerAgeValue > 0 && numberOfKmValue > 0 && passengerNameValue.length > 0) {
+        // se la verifica è andata a buon fine e i dati sono corretti, procedo con il calcolo del biglietto in base all'età
 
         if (passengerAgeValue < 18) { // se minorenne
             ticketFinalPrice = basePrice - (basePrice * 0.2);
@@ -49,58 +49,63 @@ formEL.addEventListener("submit", function (e) {
             ticketFinalPrice = basePrice;
             typeOfOffer = "Biglietto standard"
         }
+        // dopo il calcolo del biglietto procedo con il generare il biglietto completo
 
-    } else {
-        alert("Dati non validi")
-    }
-
-    // generate a random number
-    let carriageNumber = Math.floor((Math.random() * 10) + 1);
-    let tripCodeNumber = Math.floor((Math.random() * 99999) + 1);
-
-    // cambiare la classe della colonna del forum per farla rimpicciolire
-    colEl.classList.replace("col-12", "col-5");
-
-    const tripInfoEl = `
-    <div class="card w-100" style="width: 18rem;">
-        <div class="card-body border border-warning-subtle border-1 rounded-2" style="background-color: #FFFBE9;">
-            <h5 class="card-title mb-4">${passengerNameValue}</h5>
-            <hr>
-            <div class="row mb-4">
-                <div class="col-6">
-                    <span class="fw-bold">Offerta</span>
+        // genero due numeri casuali da inserire nel markup del biglietto
+        let carriageNumber = Math.floor((Math.random() * 10) + 1);
+        let tripCodeNumber = Math.floor((Math.random() * 99999) + 1);
+    
+        // cambio la classe della colonna del forum per farla rimpicciolire quando si genera il biglietto
+        colEl.classList.replace("col-12", "col-5");
+    
+        // creo il markup da inserire nell'HTML con le variabili che rappresentano i dati dell'utente
+        const tripInfoEl = `
+        <div class="card w-100" style="width: 18rem;">
+            <div class="card-body border border-warning-subtle border-1 rounded-2" style="background-color: #FFFBE9;">
+                <h5 class="card-title mb-4">${passengerNameValue}</h5>
+                <hr>
+                <div class="row mb-4">
+                    <div class="col-6">
+                        <span class="fw-bold">Offerta</span>
+                    </div>
+                    <div class="col-6">
+                        <span>${typeOfOffer}</span>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <span>${typeOfOffer}</span>
+                <div class="row mb-4">
+                    <div class="col-6">
+                        <span class="fw-bold">Carrozza</span>
+                    </div>
+                    <div class="col-6">
+                        <span>${carriageNumber}</span>
+                    </div>
                 </div>
-            </div>
-            <div class="row mb-4">
-                <div class="col-6">
-                    <span class="fw-bold">Carrozza</span>
+                <div class="row mb-4">
+                    <div class="col-6">
+                        <span class="fw-bold">Codice viaggio</span>
+                    </div>
+                    <div class="col-6">
+                        <span>${tripCodeNumber}</span>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <span>${carriageNumber}</span>
-                </div>
-            </div>
-            <div class="row mb-4">
-                <div class="col-6">
-                    <span class="fw-bold">Codice viaggio</span>
-                </div>
-                <div class="col-6">
-                    <span>${tripCodeNumber}</span>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <span class="fw-bold">Prezzo biglietto</span>
-                </div>
-                <div class="col-6">
-                    <span>${ticketFinalPrice.toFixed(2)}€</span>
+                <div class="row">
+                    <div class="col-6">
+                        <span class="fw-bold">Prezzo biglietto</span>
+                    </div>
+                    <div class="col-6">
+                        <span>${ticketFinalPrice.toFixed(2)}€</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
+        `;
+    
+        // aggiungo il markup all'HTML
+        cardsEL.insertAdjacentHTML("beforeend", tripInfoEl);
 
-    cardsEL.insertAdjacentHTML("beforeend", tripInfoEl);
+    } else { // se i dati non sono corretti, resetto il form e esco dalla funzione
+        alert("Dati non validi, riprova")
+        formEL.reset();
+        return;
+    }
 });
